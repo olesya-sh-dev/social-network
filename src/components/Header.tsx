8443ffc7e85f6982a5025cd31cc1afb755451bca
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-06 13:20:49 +0300

    верстка, создание компонентов

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://cdn4.vectorstock.com/i/1000x1000/46/83/samurai-logo-vector-31294683.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div className="cont">
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
