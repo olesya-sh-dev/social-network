83701bdb84964ded6b566d243a638631cdf2ad45
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-05-28 20:00:23 +0300

    UserAPIComp and UserFunc

--- src/components/Users/UsersAPIComponent.tsx
+++ src/components/Users/UsersAPIComponent.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import s from "./Users.module.css";
-import { Button } from "../Button";
 import axios from "axios";
 import { UsersMapPropsType } from "./UsersContainer";
+import { UsersFunc } from "./UsersFunc";
 
 class UsersAPIComponent extends React.Component<UsersMapPropsType> {
   // если кроме super в конструкторе ничего нет, то можно не писать, он идет по умолчанию
@@ -34,68 +33,10 @@ class UsersAPIComponent extends React.Component<UsersMapPropsType> {
       });
   };
   render() {
-    //console.log(this.props.totalUsersCount);
-    //console.log(this.props.pageSize);
-    let pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize
-    );
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-    return (
-      <>
-        <div className={s.pages}>
-        
-          {pages.map((p) => {
-            return <span className={this.props.currentPage===p ? s.selectedPage: ""} onClick = {(e) => {this.onPageChanged(p)}} key={p}>{p}</span>;
-          })}
-        </div>
-        {this.props.users.map((u, index) => (
-          <div className={s.userBlock} key={index}>
-            <div className={s.colomnInfo}>
-              <img
-                className={s.userImg}
-                src={
-                  u.photos.small
-                    ? u.photos.small
-                    : "https://img.freepik.com/free-psd/3d-render-cat-emoji_23-2150311907.jpg?w=826&t=st=1716839538~exp=1716840138~hmac=9613992c138d69655f2378a3d65428a9cc6141be5eccdb37d61a5cd1f6cb4b54"
-                }
-                alt="img"
-              />
-              {u.followed ? (
-                <Button
-                  onClick={() => {
-                    this.props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </Button>
-              ) : (
-                <Button
-                  onClick={() => {
-                    this.props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </Button>
-              )}
-            </div>
+   
+    return <UsersFunc totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize} users={this.props.users} currentPage={this.props.currentPage} onPageChanged={this.onPageChanged} follow={this.props.follow} unfollow={this.props.unfollow}/>
 
-            <div className={s.userInfo}>
-              <div className={s.colomnInfo}>
-                <div>{u.name}</div>
-                <span>{u.status}</span>
-              </div>
-              <div className={s.colomnInfo}>
-                <span>{"u.location.city"}</span>
-                <span>{"u.location.country"}</span>
-              </div>
-            </div>
-          </div>
-        ))}
-      </>
-    );
+ 
   }
 }
 
---
+++ src/components/Users/UsersFunc.tsx
@@ -0,0 +1,74 @@
+import { Button } from "../Button";
+import { UserType } from "../redux/users-reducer";
+import s from "./Users.module.css";
+
+
+export const UsersFunc = (props: {totalUsersCount: number, pageSize: number, currentPage: number, users: UserType[], follow: (userId: number) => void, unfollow: (userId: number) => void, onPageChanged: (pageNumber: number) => void }) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
+  return (
+    <>
+      <div className={s.pages}>
+        {pages.map((p) => {
+          return (
+            <span
+              className={props.currentPage === p ? s.selectedPage : ""}
+              onClick={(e) => {
+                props.onPageChanged(p);
+              }}
+              key={p}
+            >
+              {p}
+            </span>
+          );
+        })}
+      </div>
+      {props.users.map((u, index) => (
+        <div className={s.userBlock} key={index}>
+          <div className={s.colomnInfo}>
+            <img
+              className={s.userImg}
+              src={
+                u.photos.small
+                  ? u.photos.small
+                  : "https://img.freepik.com/free-psd/3d-render-cat-emoji_23-2150311907.jpg?w=826&t=st=1716839538~exp=1716840138~hmac=9613992c138d69655f2378a3d65428a9cc6141be5eccdb37d61a5cd1f6cb4b54"
+              }
+              alt="img"
+            />
+            {u.followed ? (
+              <Button
+                onClick={() => {
+                  props.unfollow(u.id);
+                }}
+              >
+                Unfollow
+              </Button>
+            ) : (
+              <Button
+                onClick={() => {
+                  props.follow(u.id);
+                }}
+              >
+                Follow
+              </Button>
+            )}
+          </div>
+
+          <div className={s.userInfo}>
+            <div className={s.colomnInfo}>
+              <div>{u.name}</div>
+              <span>{u.status}</span>
+            </div>
+            <div className={s.colomnInfo}>
+              <span>{"u.location.city"}</span>
+              <span>{"u.location.country"}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
