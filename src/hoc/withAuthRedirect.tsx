4df9d8acb1e10ac0fe71e6f67b59ce31c053310b
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-06-13 16:06:40 +0300

    refactoring withAuthRedirect - FC, typescript

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,30 +1,52 @@
-import React, { FC } from "react";
+import React, { ComponentType} from "react";
 import { Navigate} from "react-router-dom";
 import { AppStateType } from "../components/redux/redux-store";
 import { connect } from "react-redux";
 
-type RedirectComponentProps = {
-  isAuth: boolean;
-  // другие пропсы...
+
+type MapStatePropsType = {
+  isAuth: boolean
+
 }
- let mapStateToPropsForRedirect = (state: AppStateType) => {
-    return {
-      isAuth: state.auth.isAuth
-    }
+const mapStateToPropsForRedirect = (state: AppStateType): MapStatePropsType => {
+  return {
+    isAuth: state.auth.isAuth
+
   }
-export const withAuthRedirect = (Component: any) => {
-    
-    class RedirectComponent extends React.Component<RedirectComponentProps> {
-        render() {
-            if (!this.props.isAuth) return <Navigate to="/login" />
-            return <Component {...this.props} />
-        }
-    }
-
-    let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent);
-    return ConnectedAuthRedirectComponent
 }
+export function withAuthRedirect <T>(Component: ComponentType<T>) {
+
+  const RedirectComponent = (props: MapStatePropsType)=> {
+    let {isAuth, ...restProps} = props
+    if (!isAuth) return <Navigate to="/login" />
+    return <Component {...restProps  as T & {}}/>
+  }
+
+  let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent);
+
+  return ConnectedAuthRedirectComponent
+}
+    
+// type RedirectComponentProps = {
+//   isAuth: boolean;
+  
+// }
+//  let mapStateToPropsForRedirect = (state: AppStateType) => {
+//     return {
+//       isAuth: state.auth.isAuth
+//     }
+//   }
+    // class RedirectComponent extends React.Component<RedirectComponentProps> {
+    //     render() {
+    //         if (!this.props.isAuth) return <Navigate to="/login" />
+    //         return <Component {...this.props} />
+    //     }
+    // }
+
+    // let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent);
+    // return ConnectedAuthRedirectComponent
+//}
 
 
-// не работает с редиректом. Это классовый компонент.Нужен простой компонент. Или все страницы поместить в AuthRedirectComponent В КАЖДОМ КОМПОНЕНТЕ
+
         
